e4216b65a0f61614ff1b7a03756b5acf0c08a9fd
khuweeming <68592706+blank-debug@users.noreply.github.com>
2022-08-30 10:45:19 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cjj1z4ah4uho.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "khuweeming-employee"
+customregion = "us-east-1"
